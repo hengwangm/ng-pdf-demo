1001ef70040a0b16a186e4cf8eceeefe910f4d86
Wang Heng <hengwangm@isoftstone.com>
2017-08-02 18:33:57 +0800

    edit

--- main.js
+++ main.js
@@ -3,36 +3,8 @@
  */
 var app = angular.module('App', ['pdf']);
 app.controller('DocCtrl', function($scope,$timeout,myService) {
-	  var a=location.search.replace('?','').split('=');
-	  if(a.indexOf('kb_id') != -1){
-		  var param=a[1];
-		  myService.getPdf1(param).then(function(data){
-			  if(data.success){
-				  /*利用a标签的download属性可以完成下载
-				   *var a = document.getElementById("downPdf");  
-	                a.href='../../'+data.onlinePath;  
-	                a.download='ss.img';  
-	                a.click(); */ 
-				  //直接利用浏览器自带的paf插件打开也可以的
-				  //window.open('../../'+data.onlinePath);
-				  $scope.pdfUrl = '../../'+data.onlinePath;
-			  }else{
-				  $scope.loading = '操作失败,'+data.msg;
-			  }
-			  
-		  });
-	  }else{
-		  var param=a[1];
-		  myService.getPdf2(param).then(function(data){
-			  if(data.success){
-				  //window.open('../../'+data.onlinePath);
-				  $scope.pdfUrl = '../../'+data.onlinePath ;
-			  }else{
-				  $scope.loading =  '操作失败,'+data.msg;
-			  }
-		  });
-	  }
 	  
+	  $scope.pdfUrl ='...'
 	  $scope.scroll = 0;
 	  $scope.loading = '加载中...';
 
@@ -61,11 +33,7 @@ app.controller('DocCtrl', function($scope,$timeout,myService) {
 	    		return resp.data;
 	    	});
 	    }
-	    this.getPdf2 = function(param){
-	    	return $http.get('/ovu-pcos/pcos/contractManagement/read.do?contractId='+param).then(function(resp) {
-	    		return resp.data;
-	    	});
-	    }
-	            
+	   
+	          
 	
 	 }]);
